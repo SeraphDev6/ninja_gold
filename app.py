762a8764f038263a3036962d605e668631259973
SeraphDev6 <wesley.giles@gmail.com>
2021-08-12 23:03:38 -0400

    added reset button and made events list in descending order

--- app.py
+++ app.py
@@ -16,32 +16,35 @@ def process_money():
     if request.form['building'] == 'farm':
         earned=randint(10,20)
         session['gold'] = str(int(session['gold'])+ earned)
-        session['events'].append(f"<li class='text-success'>{now}: Earned {earned} gold From the Farm!</li>")
+        session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the Farm!</li>")
     elif request.form['building'] == 'cave':
         earned=randint(5,10)
         session['gold'] = str(int(session['gold'])+ earned)
-        session['events'].append(f"<li class='text-success'>{now}: Earned {earned} gold From the Cave!</li>")
+        session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the Cave!</li>")
     elif request.form['building'] == 'house':
         earned=randint(2,5)
         session['gold'] = str(int(session['gold'])+ earned)
-        session['events'].append(f"<li class='text-success'>{now}: Earned {earned} gold From the House!</li>")
+        session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the House!</li>")
     elif request.form['building'] == 'casino':
         earned=randint(-50,min(int(session['gold']),50))
         if earned > 0:
-            session['events'].append(f"<li class='text-success'>{now}: Earned {earned} gold From the Casino! Lucky!!!</li>")
+            session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the Casino! Lucky!!!</li>")
             session['gold'] = str(int(session['gold'])+ earned)
         elif earned == 0:
-            session['events'].append(f"<li>{now}: You broke even at the Casino! Oh well.</li>")
+            session['events'].insert(0,f"<li>{now}: You broke even at the Casino! Oh well.</li>")
         elif earned < 0:
             if abs(earned) >= int(session['gold']):
-                session['events'].append(f"<li class='text-danger'>{now}: Lost all of your gold at the Casino!!! That's why we don't gamble...</li>")
+                session['events'].insert(0,f"<li class='text-danger'>{now}: Lost all of your gold at the Casino!!! That's why we don't gamble...</li>")
                 session['gold']=0
             else:
-                session['events'].append(f"<li class='text-danger'>{now}: Lost {abs(earned)} gold at the Casino! Ouch...</li>")
+                session['events'].insert(0,f"<li class='text-danger'>{now}: Lost {abs(earned)} gold at the Casino! Ouch...</li>")
                 session['gold'] = str(int(session['gold'])+ earned)
     print(session['events'])
     return redirect('/')
-
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 
 if __name__ == '__main__':
