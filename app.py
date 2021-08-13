2ea1075808ba62c2e6cf8228814df1937144e455
SeraphDev6 <wesley.giles@gmail.com>
2021-08-12 23:12:15 -0400

    refactored app code and changed some text wording

--- app.py
+++ app.py
@@ -10,22 +10,12 @@ def index():
         session['events']=[]
     return render_template('index.html')
 
+buildings={'farm':20,'cave':10,'house':5}
+
 @app.route('/process_money', methods=['POST'])
 def process_money():
     now = datetime.datetime.now().strftime("%m/%d/%Y, at %H:%M %p")
-    if request.form['building'] == 'farm':
-        earned=randint(10,20)
-        session['gold'] = str(int(session['gold'])+ earned)
-        session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the Farm!</li>")
-    elif request.form['building'] == 'cave':
-        earned=randint(5,10)
-        session['gold'] = str(int(session['gold'])+ earned)
-        session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the Cave!</li>")
-    elif request.form['building'] == 'house':
-        earned=randint(2,5)
-        session['gold'] = str(int(session['gold'])+ earned)
-        session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the House!</li>")
-    elif request.form['building'] == 'casino':
+    if request.form['building'] == 'casino':
         earned=randint(-50,min(int(session['gold']),50))
         if earned > 0:
             session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the Casino! Lucky!!!</li>")
@@ -39,7 +29,11 @@ def process_money():
             else:
                 session['events'].insert(0,f"<li class='text-danger'>{now}: Lost {abs(earned)} gold at the Casino! Ouch...</li>")
                 session['gold'] = str(int(session['gold'])+ earned)
-    print(session['events'])
+    else:
+        earned=randint(buildings[request.form['building']]//2,buildings[request.form['building']])
+        session['events'].insert(0,f"<li class='text-success'>{now}: Earned {earned} gold From the {request.form['building']}!</li>")
+        session['gold'] = str(int(session['gold'])+ earned)
+
     return redirect('/')
 @app.route('/reset')
 def reset():
